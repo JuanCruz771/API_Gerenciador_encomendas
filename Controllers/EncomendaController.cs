07a67b814aea8b4808a2296e71e72186895e0f60
Juan Gabriel <gabrielcruz.juan.771@gmail.com>
2026-04-21 21:48:43 -0300

    Base

---
+++ Controllers/EncomendaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace API_Gerendiador_Encomendas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EncomendaController : ControllerBase
+    {
+        // GET: api/<EncomendaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<EncomendaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<EncomendaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<EncomendaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<EncomendaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
